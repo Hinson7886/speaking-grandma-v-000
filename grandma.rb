e6f7dae9a348345ce84d498b9dde984781a28785
Jon Hinson <jonhinsonfx@gmail.com>
2017-11-23 00:48:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
      return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == "WHAT DID YOU EAT TODAY?".upcase
        return "NO, NOT SINCE 1938!"
-   else phrase == "I LOVE YOU GRANDMA!"
+   else phrase == "I LOVE YOU GRANDMA!".upcase
      return "I LOVE YOU TOO PUMPKIN!"
 end
  end
